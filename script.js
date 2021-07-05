b209dce1112ba49eb2902d20496d3c932e270a19
ArisC1 <67166970+Aris-C@users.noreply.github.com>
2021-07-05 23:13:35 +0000

    started it

--- script.js
+++ script.js
@@ -1,12 +1,37 @@
-// Author:
+// Author: Aris Carter
 
 // Global UI Variables
-//let canvasDiv;
+let canvasDiv;
+let canvasDiv;
+let canvas;
+let textDiv;
+let textP;
+let textP2;
+let buttonDiv;
+let upButton;
+let downButton;
+let leftButton;
+let rightButton;
 
 // Global ML variables
-//let featureExtractor;
+let featureExtractor;
+let featureExtractor;
+let imgFeatures;
+let knnClassifier;
+let video;
+let isModelReady
+let ups;
+let downs;
+let lefts;
+let rights;
 
 function setup() {
+  canvasDiv = createDiv();
+  canvas = createCanvas(640, 480);
+  canvas.parent(canvasDiv);
+  textDiv = createDiv();
+  textP = createP();
+  textP.parent(textDiv);
 
 }
 
